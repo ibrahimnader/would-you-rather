9a269cbdc0650544cae0c245004765be49828885
ibrahimnader <ibrahimnader01@gmail.com>
2021-10-18 09:24:38 +0200

    adding login

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import UsersSlice from '../features/users/UsersSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    users:UsersSlice
   },
 });
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+               <Link  className="nav-link" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+               <Link className="nav-link"  to="/home">Answered Questions</Link>
+              </li>
+              <li className="nav-item">
+               <Link className="nav-link"  to="/home">Leader Board</Link>
+              </li>
+             
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
